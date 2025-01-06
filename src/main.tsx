a8c1f1553fefbe95fcf133b75862bbc7273f31be
Harold Camargo <hscamargo2002@gmail.com>
2025-01-06 17:30:33 -0500

    ✨ router & storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+//@LIBRERIAS
+import { PersistGate } from 'redux-persist/integration/react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+//@COMPONENTES
+import RouterComponent from './router/router.component'
+
+//@REDUCERS
+import { persistor, store } from './store'
+
+//@SASS
 import './index.scss'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+      <RouterComponent />
+  </PersistGate>
+</Provider>
 )
